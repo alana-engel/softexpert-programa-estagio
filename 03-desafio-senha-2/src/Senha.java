8ca80a1d87558f3e4f222ab40fcf65392995d8f6
Alana Engel <alana.engel@softexpert.com>
2017-05-12 10:44:09 -0300

    Arquivo atualizado com melhorias sugeridas

--- 03-desafio-senha-2/src/Senha.java
+++ 03-desafio-senha-2/src/Senha.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 class Senha{
   public static void main(String[] args) {
     boolean acertou;
-    NumerosDigitados numeroDigitado= new NumerosDigitados();
-    
+    VerificaNumeros numeros= new VerificaNumeros();
     System.out.print("Voce tem 5 tentativas para adivinhar a senha"); 
-    numeroDigitado.numerosGerados.aleatorio(); // gera os numeros 
+    numeros.numerosGerados.aleatorio(); // gera os numeros 
     for (int i=0; i<5; i++) {
       System.out.print("\nTentativa "+(i+1)); 
-      numeroDigitado.digiteSenha();
-      acertou=numeroDigitado.adivinhou();
+      numeros.numDigitados.digiteSenha();
+      numeros.comparaSenha();
+      acertou=numeros.adivinhou();
       if(acertou == true)
         break;
       else if(i==4)
-        numeroDigitado.imprime();
+        numeros.imprime();
     }
   }
 }
 
-class NumerosGerados{
+class GeraNumeros{
   ArrayList<Integer> num = new ArrayList<Integer>();
 
   void aleatorio(){ // gera os numeros aleatórios 
@@ -32,12 +32,9 @@ class NumerosGerados{
   }
 }
 
-class NumerosDigitados{
-  NumerosGerados numerosGerados = new NumerosGerados();
-  private int numerosDigitados[] = new int[5];
-  private boolean valor;
+class RecebeNumerosDigitados{
+  int numerosDigitados[] = new int[5];
   private Scanner entrada;
-  
   void digiteSenha(){ // recebe a senha digitada
     String recebe, separaNum;
     entrada = new Scanner (System.in);
@@ -49,23 +46,30 @@ class NumerosDigitados{
         separaNum=recebe.substring(i,i+1);
         numerosDigitados[i]=Integer.parseInt(separaNum);
       }
-      comparaSenha();
     }
     else{
       System.out.println("Digite 5 números");
     }
   }
+  private boolean campoNumerico(String campo){           
+    return campo.matches("[0-9]{5}"); 
+  }
+}
 
-  private void comparaSenha(){ // verifica se numeros digitados estão na senha
+class VerificaNumeros{
+  private boolean valor;
+  GeraNumeros numerosGerados=new GeraNumeros();
+  RecebeNumerosDigitados numDigitados = new RecebeNumerosDigitados();
+  void comparaSenha(){ // verifica se numeros digitados estão na senha
     valor=true;
-    for (int i=0; i<numerosDigitados.length; i++) {
+    for (int i=0; i<numDigitados.numerosDigitados.length; i++) {
       boolean a=false;
-      if(numerosDigitados[i]==(numerosGerados.num.get(i))){
-        System.out.print(numerosDigitados[i]);
+      if(numDigitados.numerosDigitados[i]==(numerosGerados.num.get(i))){
+        System.out.print(numDigitados.numerosDigitados[i]);
       }
       else{
         for (int j = 0; j < 5; j++) {
-          if ((numerosGerados.num.get(j)) == numerosDigitados[i]) {
+          if ((numerosGerados.num.get(j)) == numDigitados.numerosDigitados[i]) {
             a=true;
           }
         }
@@ -76,7 +80,7 @@ class NumerosDigitados{
           System.out.print("*");
         } 
       }
-      if(numerosDigitados[i]!=(numerosGerados.num.get(i))){
+      if(numDigitados.numerosDigitados[i]!=(numerosGerados.num.get(i))){
         valor=false;
       }
     } 
@@ -94,9 +98,5 @@ class NumerosDigitados{
     for (int i=0; i<5; i++) {
       System.out.print((numerosGerados.num.get(i))); 
     }
-  }
-
-  private boolean campoNumerico(String campo){           
-    return campo.matches("[0-9]{5}"); 
   }
 }
