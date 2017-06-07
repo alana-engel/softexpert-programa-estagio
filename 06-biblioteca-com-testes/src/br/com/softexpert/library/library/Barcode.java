fe877afc33f363ca78f4dbcca2e5aeeaa28df639
Alana Engel <alana.engel@softexpert.com>
2017-06-07 14:47:51 -0300

    Criação da classe para geração de código de barras

--- 06-biblioteca-com-testes/src/br/com/softexpert/library/library/Barcode.java
+++ 06-biblioteca-com-testes/src/br/com/softexpert/library/library/Barcode.java
@@ -1,5 +1,49 @@
 package br.com.softexpert.library.library;
 
 public class Barcode {
-		
+	private String countryCode;
+	private String companyCode;
+	private String productCode;
+	
+	private String getCountryCode(){
+		countryCode = "789";
+		return countryCode;
+	}
+	
+	private String getCompanyCode(){
+		companyCode = "12345";
+		return companyCode;
+	}
+
+	private String getProductCode(){
+		String cod ="0"+5;
+		int len = cod.length();  
+		while(len<4){
+		       cod="0"+cod;
+		       len++;
+		   }
+		productCode = cod;
+		return productCode;
+	}
+	
+	public String getChecker(){
+		String all =getCountryCode()+getCompanyCode()+getProductCode();
+		char nine = all.charAt(8);
+		char ten = all.charAt(9);
+		char eleven = all.charAt(10);
+		char twelve = all.charAt(11);
+		int sumP= 17+Character.getNumericValue(ten)+Character.getNumericValue(twelve);
+		int sumO= 22+Character.getNumericValue(nine)+Character.getNumericValue(eleven);
+		int x3=sumP*3;
+		int results=x3+sumO;
+		int c=0;
+		while(results%10!=0){
+			c++;
+			results++;
+		}
+		String checker =""+c;
+		String barcode=getCountryCode()+" "+getCompanyCode()+" "+getProductCode()+" "+checker;
+		return barcode;
+	
+	}
 }
