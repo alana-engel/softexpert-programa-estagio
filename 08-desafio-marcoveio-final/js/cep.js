0313e2ab21173639aeef924def866b290283fbc1
Alana Engel <alana.engel@softexpert.com>
2017-06-20 14:45:37 -0300

    Realizadas as melhorias sugeridas

--- 08-desafio-marcoveio-final/js/cep.js
+++ 08-desafio-marcoveio-final/js/cep.js
@@ -1,33 +1,40 @@
 var timer;
  $('#find').on('click', function() {
-        searchCEP();
+        var idCep = $('#cep'),
+        result = $('#comochegar'),
+        webServiceDomain = "https://viacep.com.br/ws/",
+        typeData = "/json/?callback=?";
+        searchCEP(idCep, result, webServiceDomain, typeData);
     });
-function searchCEP() {
+
+function searchCEP(idCep, result, webServiceDomain, typeData) {
 	clearInterval(timer);
     function clean_form_cep() {
        $("#comochegar").val(" ");
     }
-    var cep = $("#cep").val().replace(/\D/g, '');
+    var cep = idCep.val().replace(/\D/g, '');
     if (cep != "") {
         var validatecep = /^[0-9]{8}$/;
         if(validatecep.test(cep)) {
-            $("#comochegar").val("...");
-            $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+            result.val("...");
+            $.getJSON(webServiceDomain+ cep +typeData, function(dados) {
                 if (!("erro" in dados)) {
-                    $("#comochegar").val("Para chegar ao CEP "+ cep+", pegue um avião em direção ao estado de " + dados.uf+" , um ônibus para a cidade de "+dados.localidade+" e encontre de carro no bairro "+dados.bairro+"a rua "+dados.logradouro+".");
+                    result.val("Para chegar ao CEP "+ cep+", pegue um avião em direção ao estado de " + dados.uf+" , um ônibus para a cidade de "+dados.localidade+" e encontre de carro no bairro "+dados.bairro+"a rua "+dados.logradouro+".");
                 }
                 else {
                    timer= setInterval(function() {clean_form_cep(); clearInterval(timer);}, 3000);
-            	   $("#comochegar").val("CEP não encontrado");
+            	   result.val("CEP não encontrado");
                 }
             });
         } 
         else {
             timer= setInterval(function() {clean_form_cep(); clearInterval(timer);}, 3000);
-            $("#comochegar").val("CEP não encontrado");
+            result.val("CEP não encontrado");
         } 
     }
     else {
         clean_form_cep();
     }  
 }
+
+
